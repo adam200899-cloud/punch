91c3e4be072672df2035158adc32ff931c0b3685
adam200899-cloud <adam200899@gmail.com>
2026-09-12 22:25:03 +0800

    Create config.js

---
+++ v2/config.js
@@ -0,0 +1,4 @@
+// 把 Apps Script「部署 → 網頁應用程式」拿到的 /exec 網址貼在這裡
+window.PUNCH_CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbx6VoiUYQociWfWYdn41vBR3MalFZAq1qMNNzOiuxExHhiInXHN1ptB7Wtk1K-XC6vYww/exec',
+};
